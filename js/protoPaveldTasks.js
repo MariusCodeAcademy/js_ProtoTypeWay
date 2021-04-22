6d3b719eaa47b51376fedace2f937e66fcd990b1
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-04-22 14:10:11 +0300

    avg

--- js/protoPaveldTasks.js
+++ js/protoPaveldTasks.js
@@ -15,25 +15,32 @@ LivingThing.prototype.born = function () {
 // argumentu paduosi vardas ir email
 // metodas personInfo kuris atspausdina varda email ir kiek metu
 // metodas email() kuris grazina "noredami siusti el pasta <vardas> siuskite <email> adresu"
-function Person(name, email, birthYear) {
-  LivingThing.call(this, birthYear);
-  this.vardas = name;
-  this.elPastas = email;
+{
+  function Person(name, email, birthYear) {
+    LivingThing.call(this, birthYear);
+    this.vardas = name;
+    this.elPastas = email;
+  }
+  Person.prototype = Object.create(LivingThing.prototype);
+  Person.prototype.amzius = function () {
+    let now = new Date();
+    let amzius = now.getFullYear() - this.birthYear;
+    return amzius;
+  };
+  Person.prototype.personInfo = function () {
+    console.log(`Vardas: ${this.vardas}, elPastas: ${this.elPastas}  amzius: ${this.amzius()} `);
+  };
+  Person.prototype.email = function () {
+    console.log(`noredami siusti el pasta ${this.vardas} siuskite ${this.elPastas} adresu`);
+  };
+  console.group("Person");
+  const per1 = new Person("James", "jj@j.com", 1980);
+  per1.personInfo();
+  per1.born();
+  per1.email();
+  console.groupEnd();
+  console.log("");
 }
-Person.prototype = Object.create(LivingThing.prototype);
-Person.prototype.personInfo = function () {
-  let now = new Date();
-  let amzius = now.getFullYear() - this.birthYear;
-  console.log(`Vardas: ${this.vardas}, elPastas: ${this.elPastas}  amzius: ${amzius} `);
-};
-Person.prototype.email = function () {
-  console.log(`noredami siusti el pasta ${this.vardas} siuskite ${this.elPastas} adresu`);
-};
-
-const per1 = new Person("James", "jj@j.com", 1980);
-per1.personInfo();
-per1.born();
-per1.email();
 
 // 3. sukurti Student konstruktoriaus funkcija praplecia Person;
 // papildomai prideam savybe Universitas.
@@ -46,17 +53,28 @@ function Student(name, email, birthYear, university, grades) {
   this.grades = grades;
 }
 Student.prototype = Object.create(Person.prototype);
-Student.prototype.avg = (function () {
+Student.prototype.avg = function () {
   // pasiimam pazymiu masyva ir
   // apskaiciuojam vidurki
-  console.log("vidurkis");
-})();
+  let ilgis = this.grades.length;
+  let avg = this.grades.reduce((totalAvg, num) => totalAvg + num / ilgis, 0);
+  console.log("vidurkis", avg);
+  return avg;
+};
+Student.prototype.personInfo = function () {
+  console.log(`Studentas: ${this.vardas}, elPastas: ${this.elPastas}  
+  amzius: ${this.amzius()} studijuoja ${this.uni} ir jo vidurkis yra ${this.avg().toFixed(2)}`);
+};
+console.group("Student");
 let pazymiai = [6, 8, 8, 4, 6, 10, 9];
 const st1 = new Student("Mike", "mike@tyson.com", 1965, "KTU", pazymiai);
 console.log("st1", st1);
 st1.email();
 st1.personInfo();
-st1.avg;
+// console.clear();
+st1.avg();
+console.groupEnd();
+console.log("");
 
 // 4. sukurti konstruktoriaus funkcija Zinduolis praplecia LivingThing
 // pridedam savybe rusis
@@ -66,7 +84,7 @@ function Zinduolis(rusis, ...args) {
   this.rusis = rusis;
 }
 const zin1 = new Zinduolis("Katinas", 2010);
-console.log("zin1", zin1);
+// console.log("zin1", zin1);
 
 // 5 sukurti 2 LivingThingus, 3 Zinduolius, atspausdinti zinduoliu info
 // 6 sukurti 2 Person, 4 Studentus masyve. atspausdinti personInfo Studentu ir Asmenu
