157a4a7eb142ba161ce09cf279622d89a39151ba
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-04-22 10:07:34 +0300

    paveld task proto

--- js/protoPaveldTasks.js
+++ js/protoPaveldTasks.js
@@ -1,1 +1,20 @@
 "use strict;";
+console.log("paveld Proto Tasks");
+
+// neaudoti CLASS zodelio
+// 1. Sukurti LivingThing clase
+// argumentu uzpildyti birthYear savybe
+
+// 2. sukurti Person clase kuri paveldi LivingThing
+// argumentu paduosi vardas ir email
+// metodas personInfo kuris atspausdina varda email ir kiek metu
+
+// 3. sukurti Student class praplecia Person;
+// papildomai prideam savybe Universitas.
+// ir masyva pazymiai (sk masyvas apie 8 el ilgio)
+// metodas paskaiciuoti vidurkui
+// perrasyti personInfo kad prideti is kokio universiteto ir koks vidurkis
+
+// 4. sukurti klase Zinduolis praplecia LivingThing
+// pridedam savybe rusis
+// metodas info atspaudina koks amziu ir kokios rusies zinduolis
