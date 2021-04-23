e2a2382b6c1adec04da02a69f9d4ca15f35b84bf
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-04-23 11:19:45 +0300

    cars

---
+++ js/cars.js
@@ -0,0 +1,10 @@
+const cars = [
+  new Car("Audi", "A4", 2000, "black", ["dysel"], 1200),
+  new Car("Audi", "A5", 2013, "red", ["petrol"], 5000),
+  new Car("Audi", "A6", 2014, "blue", ["electric", "gas", "petrol"], 6500),
+  new Car("Audi", "A4", 2016, "white", ["dysel"], 8000),
+  new Car("Toyta", "Avensis", 2018, "black", ["dysel", "electric"], 13000),
+  new Car("Audi", "A7", 2011, "red", ["petrol"], 4000),
+  new Car("Nisan", "Z", 2015, "blue", ["petrol", "gas"], 4000),
+  new Car("Audi", "A6", 2020, "white", ["petrol", "electric"], 24000),
+];
