9549815b3617ca37666e6a39030bc6c1be3557fe
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-04-23 08:33:55 +0300

    iter 2

--- js/protoPaveldTasks.js
+++ js/protoPaveldTasks.js
@@ -33,7 +33,7 @@ LivingThing.prototype.born = function () {
   Person.prototype.email = function () {
     console.log(`noredami siusti el pasta ${this.vardas} siuskite ${this.elPastas} adresu`);
   };
-  console.group("Person");
+  console.groupCollapsed("Person");
   const per1 = new Person("James", "jj@j.com", 1980);
   per1.personInfo();
   per1.born();
@@ -65,7 +65,7 @@ Student.prototype.personInfo = function () {
   console.log(`Studentas: ${this.vardas}, elPastas: ${this.elPastas}  
   amzius: ${this.amzius()} studijuoja ${this.uni} ir jo vidurkis yra ${this.avg().toFixed(2)}`);
 };
-console.group("Student");
+console.groupCollapsed("Student");
 let pazymiai = [6, 8, 8, 4, 6, 10, 9];
 const st1 = new Student("Mike", "mike@tyson.com", 1965, "KTU", pazymiai);
 console.log("st1", st1);
@@ -83,11 +83,32 @@ function Zinduolis(rusis, ...args) {
   LivingThing.apply(this, args);
   this.rusis = rusis;
 }
-const zin1 = new Zinduolis("Katinas", 2010);
+// const zin1 = new Zinduolis("Katinas", 2010);
 // console.log("zin1", zin1);
-
+console.groupCollapsed("Animals");
 // 5 sukurti 2 LivingThingus, 3 Zinduolius, atspausdinti zinduoliu info
+let lt1 = new LivingThing(1900);
+let lt2 = new LivingThing(1950);
+let ltArr = [1900, 1950, 1952].map((lt) => new LivingThing(lt));
+console.log("ltArr", ltArr);
+
+let zin1 = new Zinduolis("Briedis", 2000);
+let zin2 = new Zinduolis("Stirna", 2001);
+let zin3 = new Zinduolis("Sheep", 2010);
+console.log(zin1, zin2, zin3);
+console.groupEnd();
+console.log("");
+
+console.group("6 sukurti 2 Person, 4 Studentus masyve. atspausdinti personInfo Studentu ir Asmenu");
 // 6 sukurti 2 Person, 4 Studentus masyve. atspausdinti personInfo Studentu ir Asmenu
+const studentsArr = [
+  new Student("Mike", "mike@tyson.com", 1965, "KTU", [5, 7, 6]),
+  new Student("James", "James@Blue.com", 1995, "VGTU", [4, 10, 10]),
+  new Student("Marry", "Marry@Blue.com", 2005, "VGTU", [10, 10, 10]),
+  new Student("Bob", "Bob@Stone.com", 2010, "VGTU", [7, 8, 7]),
+];
+console.groupEnd();
+console.log("");
 // 7 sukurtiem Studentam iskviesti Email metoda ciklo budu
 // 8 parasyti funkcija kuri pasiima studentu masyva ir grazina kiekvieno studento varda ir
 // vidurki objektu masyvo pavidalu
