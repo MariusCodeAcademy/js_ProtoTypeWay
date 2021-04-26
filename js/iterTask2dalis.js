0665aeeed6fac24555483474d05947a46e4f33b5
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-04-26 08:21:15 +0300

    2 dalis

---
+++ js/iterTask2dalis.js
@@ -0,0 +1,29 @@
+"use strict"; // here we go again
+
+console.log("Iter 2 ");
+
+class Person {
+  constructor({ name, surname, age, height, weight, sex }) {
+    this.name = name;
+    this.surname = surname;
+    this.age = age;
+    this.height = height;
+    this.weight = weight;
+    this.sex = sex;
+  }
+}
+
+// let testObj = {
+//   name: "Arananas",
+//   surname: "Baras",
+//   age: 7,
+//   height: 110,
+//   weight: 40,
+//   sex: "male",
+// };
+
+// let pers1 = new Person(testObj);
+
+const personsArr = peopleObjs.map(() => {});
+
+console.log("personsArr", personsArr);
