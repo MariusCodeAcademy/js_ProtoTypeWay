e8d60313e32d70708b758782bbba85b4e9dd1c9d
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-04-26 09:31:19 +0300

    yes

--- js/iterTask2dalis.js
+++ js/iterTask2dalis.js
@@ -11,6 +11,22 @@ class Person {
     this.weight = weight;
     this.sex = sex;
   }
+  //vidinis kmi metodas
+  get kmi() {
+    let kmiLong = this.weight / (this.height / 100) ** 2;
+    return +kmiLong.toFixed(2);
+  }
+  toString() {
+    let text = `
+      Vardas: ${this.name}, 
+      Pavarde: ${this.surname}, 
+      Amzius: ${this.age}, 
+      Ugis: ${this.height}, 
+      Svoris: ${this.weight}, 
+      Lytis: ${this.sex},
+      `;
+    console.log(text);
+  }
 }
 
 // let testObj = {
@@ -22,8 +38,41 @@ class Person {
 //   sex: "male",
 // };
 
+const personsArr = peopleObjs.map((perObj) => new Person(perObj));
 // let pers1 = new Person(testObj);
+// console.table(personsArr);
+console.groupCollapsed(
+  "0. Pasinaudojant 1 dalies elementų masyvu, sukurti Person prototipų(Class) masyvą:"
+);
 
-const personsArr = peopleObjs.map(() => {});
+let pirmas = personsArr[0];
 
-console.log("personsArr", personsArr);
+console.log(pirmas.toString());
+console.groupEnd();
+console.log("");
+
+console.groupCollapsed(
+  "1. Atrinkti moteris, kuriuos jaunesnės nei 20 metų ir svoris didesnis nei 70kg "
+);
+// separate steps filter
+let moterys = personsArr.filter((persObj) => persObj.sex === "female");
+console.table(moterys);
+let jaunesnes = moterys.filter((p) => p.age < 20);
+console.table(jaunesnes);
+let svor70 = jaunesnes.filter((p) => p.weight > 70);
+console.log("svor70");
+
+// filter chain ========
+svor70 = personsArr
+  .filter((p) => p.sex === "female")
+  .filter((p) => p.age < 20)
+  .filter((p) => p.weight > 70);
+console.table(svor70);
+
+console.groupEnd();
+console.log("");
+
+console.group("2. Atrinkti vyrus, kurie vyresni nei 25 metai ir KMI mažesnis nei 18,5");
+
+console.groupEnd();
+console.log("");
